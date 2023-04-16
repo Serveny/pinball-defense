c3446b439cf84deeb3d6a57c04bbb941528b52ec
Serveny <dylan_lewis93@web.de>
2023-04-16 10:27:39 +0200

    models organized

--- src/assets.rs
+++ src/assets.rs
@@ -2,27 +2,28 @@ use crate::prelude::*;
 
 #[derive(Resource, AssetCollection)]
 pub struct PinballDefenseAssets {
-    #[asset(path = "models/pinball_world_1.gltf#Mesh1/Primitive0")]
+    #[asset(path = "models/pinball_plate_1.gltf#Mesh0/Primitive0")]
     pub world_1_mesh: Handle<Mesh>,
 
-    #[asset(path = "models/pinball_world_1.gltf#Mesh0/Primitive0")]
+    #[asset(path = "models/flippers.gltf#Mesh0/Primitive0")]
     pub flipper_right: Handle<Mesh>,
 
-    #[asset(path = "models/pinball_world_1.gltf#Mesh2/Primitive0")]
+    #[asset(path = "models/flippers.gltf#Mesh1/Primitive0")]
     pub flipper_left: Handle<Mesh>,
 
-    #[asset(path = "models/pinball_world_1.gltf#Mesh3/Primitive0")]
+    // Maybe different bases?
+    #[asset(path = "models/towers/tower_microwave.gltf#Mesh0/Primitive0")]
     pub tower_base: Handle<Mesh>,
 
-    #[asset(path = "models/pinball_world_1.gltf#Mesh4/Primitive0")]
+    #[asset(path = "models/towers/tower_microwave.gltf#Mesh1/Primitive0")]
     pub tower_microwave_top: Handle<Mesh>,
 
-    #[asset(path = "models/pinball_world_1.gltf#Mesh5/Primitive0")]
+    #[asset(path = "models/towers/tower_machine_gun.gltf#Mesh1/Primitive0")]
     pub tower_mg_mounting: Handle<Mesh>,
 
-    #[asset(path = "models/pinball_world_1.gltf#Mesh6/Primitive0")]
+    #[asset(path = "models/towers/tower_machine_gun.gltf#Mesh2/Primitive0")]
     pub tower_mg_head: Handle<Mesh>,
 
-    #[asset(path = "models/pinball_world_1.gltf#Mesh7/Primitive0")]
+    #[asset(path = "models/towers/tower_machine_gun.gltf#Mesh3/Primitive0")]
     pub tower_mg_barrel: Handle<Mesh>,
 }
