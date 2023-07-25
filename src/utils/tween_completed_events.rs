5104a31694c1e96968e0c52d9074d67cfbea1a92
Serveny <dylan_lewis93@web.de>
2023-07-25 17:13:02 +0200

    Menu WIP

--- src/utils/tween_completed_events.rs
+++ src/utils/tween_completed_events.rs
@@ -1,13 +1,12 @@
 use crate::prelude::*;
 use bevy_tweening::TweenCompleted;
 
-pub const DESPAWN_ENTITY_AND_MENU_EVENT_ID: u64 = 0;
-//pub const PINBALL_MENU_SPAWN_ANIMATION_END_EVENT: u64 = 1;
+pub const DESPAWN_ENTITY_EVENT_ID: u64 = 0;
 
 pub(super) fn tween_completed_system(mut evr: EventReader<TweenCompleted>, mut cmds: Commands) {
     for ev in evr.iter() {
         match ev.user_data {
-            DESPAWN_ENTITY_AND_MENU_EVENT_ID => cmds.entity(ev.entity).despawn_recursive(),
+            DESPAWN_ENTITY_EVENT_ID => cmds.entity(ev.entity).despawn_recursive(),
             _ => panic!("😭 Unkown tween user event: {}", ev.user_data),
         }
     }
