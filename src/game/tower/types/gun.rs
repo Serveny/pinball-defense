c2714255579291a25ca02db4400cd156e14968a5
Serveny <dylan_lewis93@web.de>
2023-08-20 11:26:21 +0200

    Little muzzle flash animation

--- src/game/tower/types/gun.rs
+++ src/game/tower/types/gun.rs
@@ -40,8 +40,13 @@ pub fn spawn(
             let rel_id = tower.parent_entity();
 
             // Children of tower
+            let muzzle_flash_light = |parent: &mut ChildBuilder| {
+                parent.spawn(muzzle_flash_light(g_sett, rel_id));
+            };
             let mg_barrel = |parent: &mut ChildBuilder| {
-                parent.spawn(barrel(tower_mat.clone(), assets));
+                parent
+                    .spawn(barrel(tower_mat.clone(), assets, rel_id))
+                    .with_children(muzzle_flash_light);
             };
             let mg_head = |parent: &mut ChildBuilder| {
                 parent
@@ -59,7 +64,36 @@ pub fn spawn(
     );
 }
 
-fn barrel(tower_mat: Handle<StandardMaterial>, assets: &PinballDefenseAssets) -> impl Bundle {
+#[derive(Component)]
+pub struct MuzzleFlashLight;
+
+fn muzzle_flash_light(g_sett: &GraphicsSettings, rel_id: Entity) -> impl Bundle {
+    (
+        SpotLightBundle {
+            transform: Transform::from_translation(Vec3::new(0., 0., -0.04))
+                .looking_at(Vec3::new(0.0, 0.0, -1.0), Vec3::Z),
+            spot_light: SpotLight {
+                intensity: 0., // lumens - roughly a 100W non-halogen incandescent bulb
+                color: Color::rgba_u8(215, 205, 117, 255),
+                shadows_enabled: g_sett.is_shadows,
+                range: 0.4,
+                inner_angle: 0.2,
+                outer_angle: 0.8,
+                ..default()
+            },
+            visibility: Visibility::Hidden,
+            ..default()
+        },
+        MuzzleFlashLight,
+        RelEntity(rel_id),
+    )
+}
+
+fn barrel(
+    tower_mat: Handle<StandardMaterial>,
+    assets: &PinballDefenseAssets,
+    rel_id: Entity,
+) -> impl Bundle {
     (
         PbrBundle {
             mesh: assets.tower_mg_barrel.clone(),
@@ -68,6 +102,7 @@ fn barrel(tower_mat: Handle<StandardMaterial>, assets: &PinballDefenseAssets) ->
             ..default()
         },
         GunTowerBarrel,
+        RelEntity(rel_id),
     )
 }
 
@@ -110,4 +145,54 @@ fn mounting(
         RelEntity(rel_id),
         TowerHead,
     )
+}
+
+pub(in super::super) fn shot_animation_system(
+    time: Res<Time>,
+    q_gun_tower: Query<(Entity, &AimFirstEnemy), With<GunTower>>,
+    mut q_barrel: Query<(&mut Transform, &RelEntity), With<GunTowerBarrel>>,
+    mut q_muzzle_flash: Query<
+        (&mut Visibility, &mut SpotLight, &RelEntity),
+        With<MuzzleFlashLight>,
+    >,
+) {
+    for (tower_id, enemy_id) in q_gun_tower.iter() {
+        let mut flash = get_flash(&mut q_muzzle_flash, tower_id);
+        match enemy_id.0 {
+            Some(_) => {
+                let sin = (time.elapsed_seconds() * 64.).sin();
+                *flash.0 = Visibility::Inherited;
+                get_barrel(&mut q_barrel, tower_id).0.translation.z = sin * 0.002;
+                flash.1.intensity = (sin + 1.) * 16.;
+            }
+            None => {
+                if *flash.0 != Visibility::Hidden {
+                    *flash.0 = Visibility::Hidden;
+                    get_barrel(&mut q_barrel, tower_id).0.translation.z = 0.;
+                }
+            }
+        }
+    }
+}
+
+fn get_barrel<'a>(
+    q_barrel: &'a mut Query<(&mut Transform, &RelEntity), With<GunTowerBarrel>>,
+    tower_id: Entity,
+) -> (Mut<'a, Transform>, &'a RelEntity) {
+    q_barrel
+        .iter_mut()
+        .find(|(_, rel_id)| rel_id.0 == tower_id)
+        .expect("No barrel for tower found")
+}
+fn get_flash<'a>(
+    q_muzzle_flash: &'a mut Query<
+        (&mut Visibility, &mut SpotLight, &RelEntity),
+        With<MuzzleFlashLight>,
+    >,
+    tower_id: Entity,
+) -> (Mut<'a, Visibility>, Mut<'a, SpotLight>, &'a RelEntity) {
+    q_muzzle_flash
+        .iter_mut()
+        .find(|(_, _, rel_id)| rel_id.0 == tower_id)
+        .expect("No muzzle flash for tower found")
 }
