0cc01dca7259bcc36c6f88ffda8a068f0dc48861
Serveny <dylan_lewis93@web.de>
2023-09-02 15:00:10 +0200

    audio settings menu

--- src/menu/pause.rs
+++ src/menu/pause.rs
@@ -1,10 +1,10 @@
-use super::{ButtonAction, MenuLayout};
+use super::{actions::MenuAction, tools::buttons::spawn_menu_button, MenuLayout, BACKGROUND};
 use crate::prelude::*;
 
-pub fn pause_menu_layout(mut cmds: Commands, assets: Res<PinballDefenseAssets>) {
+pub fn layout(mut cmds: Commands, assets: Res<PinballDefenseAssets>) {
     cmds.spawn((
         NodeBundle {
-            background_color: Color::rgba_u8(23, 24, 26, 120).into(),
+            background_color: BACKGROUND.into(),
             style: Style {
                 display: Display::Grid,
                 width: Val::Percent(100.),
@@ -20,7 +20,7 @@ pub fn pause_menu_layout(mut cmds: Commands, assets: Res<PinballDefenseAssets>) 
     ))
     .with_children(|p| {
         spawn_headline("Pause", p, &assets);
-        spawn_pause_menu_buttons(p, &assets);
+        spawn_buttons(p, &assets);
     });
 }
 
@@ -46,7 +46,7 @@ fn spawn_headline(text: &str, p: &mut ChildBuilder, assets: &PinballDefenseAsset
         });
 }
 
-fn spawn_pause_menu_buttons(p: &mut ChildBuilder, assets: &PinballDefenseAssets) {
+fn spawn_buttons(p: &mut ChildBuilder, assets: &PinballDefenseAssets) {
     p.spawn((NodeBundle {
         style: Style {
             display: Display::Flex,
@@ -64,45 +64,10 @@ fn spawn_pause_menu_buttons(p: &mut ChildBuilder, assets: &PinballDefenseAssets)
         .with_children(|p| {
             let margin = UiRect::default();
             let con_margin = UiRect::bottom(Val::Px(10.));
-            spawn_menu_button(ButtonAction::Continue, p, assets, con_margin);
-            spawn_menu_button(ButtonAction::Controls, p, assets, margin);
-            spawn_menu_button(ButtonAction::Graphics, p, assets, margin);
-            spawn_menu_button(ButtonAction::Sound, p, assets, margin);
-            spawn_menu_button(ButtonAction::Quit, p, assets, UiRect::top(Val::Px(10.)));
+            spawn_menu_button(MenuAction::Continue, p, assets, con_margin);
+            spawn_menu_button(MenuAction::Controls, p, assets, margin);
+            spawn_menu_button(MenuAction::Graphics, p, assets, margin);
+            spawn_menu_button(MenuAction::Sound, p, assets, margin);
+            spawn_menu_button(MenuAction::Quit, p, assets, UiRect::top(Val::Px(10.)));
         });
-}
-
-fn spawn_menu_button(
-    action: ButtonAction,
-    p: &mut ChildBuilder,
-    assets: &PinballDefenseAssets,
-    margin: UiRect,
-) {
-    p.spawn((
-        ButtonBundle {
-            style: Style {
-                width: Val::Percent(100.),
-                height: Val::Px(65.),
-                border: UiRect::bottom(Val::Px(2.0)),
-                margin,
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                ..default()
-            },
-            border_color: Color::rgb_u8(188, 148, 87).into(),
-            background_color: Color::NONE.into(),
-            ..default()
-        },
-        action,
-    ))
-    .with_children(|p| {
-        p.spawn(TextBundle::from_section(
-            action.to_string(),
-            TextStyle {
-                font: assets.menu_font.clone(),
-                font_size: 40.0,
-                color: Color::rgb_u8(255, 254, 236),
-            },
-        ));
-    });
 }
