b9e78a11ec98c3dbd4e1985cafa301053bb84665
Serveny <dylan_lewis93@web.de>
2023-09-03 08:48:32 +0200

    hit count of foundation depends on which level they got spawned

--- src/game/cfg.rs
+++ src/game/cfg.rs
@@ -1,16 +1,13 @@
 #[cfg(not(debug_assertions))]
 pub const CONFIG: PinballDefenseConfig = PinballDefenseConfig {
-    foundation_hit_progress: 1. / 30.,
     tower_hit_progress: 1. / 15.,
 };
 
 #[cfg(debug_assertions)]
 pub const CONFIG: PinballDefenseConfig = PinballDefenseConfig {
-    foundation_hit_progress: 1.,
     tower_hit_progress: 1.,
 };
 
 pub struct PinballDefenseConfig {
-    pub foundation_hit_progress: f32,
     pub tower_hit_progress: f32,
 }
