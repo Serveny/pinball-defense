6078a940654964ffebe4c84e4cc886742485d467
Serveny <dylan_lewis93@web.de>
2023-09-08 20:00:46 +0200

    Gamepad pause button

--- src/game/controls/gamepad.rs
+++ src/game/controls/gamepad.rs
@@ -4,6 +4,8 @@ use crate::game::{
     ball_starter::BallStarterState,
     flipper::{FlipperStatus, FlipperType},
 };
+use crate::game::{PauseGameEvent, ResumeGameEvent};
+use crate::menu::MenuState;
 use crate::prelude::*;
 use bevy::input::gamepad::{GamepadButtonChangedEvent, GamepadConnectionEvent};
 
@@ -12,7 +14,7 @@ use bevy::input::gamepad::{GamepadButtonChangedEvent, GamepadConnectionEvent};
 #[derive(Resource)]
 pub struct MyGamepad(pub Gamepad);
 
-pub(super) fn on_gamepad_connections(
+pub(super) fn on_dis_connect(
     mut cmds: Commands,
     my_gamepad: Option<Res<MyGamepad>>,
     mut gamepad_evr: EventReader<GamepadConnectionEvent>,
@@ -43,35 +45,53 @@ pub(super) fn on_gamepad_connections(
     }
 }
 
-pub(super) fn on_gamepad_btn_changed(
+pub(super) fn on_btn_changed(
     mut evr: EventReader<GamepadButtonChangedEvent>,
     mut spawn_ball_ev: EventWriter<SpawnBallEvent>,
     mut ball_starter_state: ResMut<NextState<BallStarterState>>,
     mut q_flipper: Query<(&mut FlipperStatus, &FlipperType)>,
+    mut menu_state: ResMut<NextState<MenuState>>,
+    mut pause_ev: EventWriter<PauseGameEvent>,
 ) {
     for ev in evr.iter() {
         match ev.button_type {
             GamepadButtonType::East if ev.value > 0. => {
                 spawn_ball_ev.send(SpawnBallEvent);
             }
-
             GamepadButtonType::South => ball_starter_state.set(match ev.value == 0. {
                 true => BallStarterState::Fire,
                 false => BallStarterState::Charge,
             }),
-
             GamepadButtonType::LeftTrigger => set_flipper_status(
                 FlipperType::Left,
                 FlipperStatus::by_value(ev.value),
                 &mut q_flipper,
             ),
-
             GamepadButtonType::RightTrigger => set_flipper_status(
                 FlipperType::Right,
                 FlipperStatus::by_value(ev.value),
                 &mut q_flipper,
             ),
-            // other events are irrelevant
+            GamepadButtonType::Start => {
+                pause_ev.send(PauseGameEvent);
+                menu_state.set(MenuState::PauseMenu);
+            }
+            _ => {}
+        }
+    }
+}
+
+pub(super) fn pause_btn_changed(
+    mut evr: EventReader<GamepadButtonChangedEvent>,
+    mut menu_state: ResMut<NextState<MenuState>>,
+    mut resume_ev: EventWriter<ResumeGameEvent>,
+) {
+    for ev in evr.iter() {
+        match ev.button_type {
+            GamepadButtonType::Start => {
+                menu_state.set(MenuState::None);
+                resume_ev.send(ResumeGameEvent);
+            }
             _ => {}
         }
     }
