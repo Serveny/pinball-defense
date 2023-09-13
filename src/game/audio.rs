d4620ba9b63e3cbd28b07d526d65f746f5c48f7c
Serveny <dylan_lewis93@web.de>
2023-09-13 17:39:29 +0200

    Ball rolling sound volume

--- src/game/audio.rs
+++ src/game/audio.rs
@@ -171,11 +171,12 @@ fn play_ball_rolling_sound(mut cmds: Commands, assets: Res<PinballDefenseAudioAs
 fn ball_rolling_sound_system(
     mut q_rolling_sound: Query<&AudioSink, With<BallRollingSound>>,
     q_ball: Query<&Velocity, With<PinBall>>,
+    sound_sett: Res<SoundSettings>,
 ) {
     if let Ok(sound) = q_rolling_sound.get_single_mut() {
         if let Some(vel) = q_ball.iter().next() {
             let linvel = vel.linvel.length().abs() / 16.;
-            sound.set_volume(linvel);
+            sound.set_volume(linvel * sound_sett.fx_volume);
             let speed = 0.9 + linvel / 2.;
             sound.set_speed(speed);
         } else {
